bdec88d504daf7d0bf5535ef7024f841b993cc65
clyons <austinlyns@gmail.com>
2019-05-24 14:52:39 -0400

    Added blake's requests for the styling of the create user modal. and added futura font to every body in the program

--- src/components/manage/create-user-modal/create-user-modal.component.tsx
+++ src/components/manage/create-user-modal/create-user-modal.component.tsx
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { ICreateUserModal } from './create-user-modal.container';
 import { IUser } from '../../../model/user.model';
+import Label from 'reactstrap/lib/Label';
 
 
 const inputNames = {
@@ -119,106 +120,123 @@ export class CreateUserModal extends React.Component<ICreateUserModal, any> {
   render() {
     const { createUser, addresses, cohorts } = this.props;
     return (
-      <Modal isOpen={this.props.createUser.enabled} id="futurafont">
+      <Modal isOpen={this.props.createUser.enabled} id="create-user-modal" className="futurafont">
         <form onSubmit={this.saveNewUser}>
           <ModalHeader className="rev-background-color">Create User</ModalHeader>
           <ModalBody>
             <div className="responsive-modal-row">
-              <Input name={inputNames.FIRST_NAME}
-                className="responsive-modal-row-item"
-                placeholder="First Name"
-                onChange={this.updateNewUserInfo}
-                value={createUser.newUser.firstName}
-                valid={!!createUser.newUser.firstName}
-                invalid={!createUser.newUser.firstName} />
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-firstname-input">First Name</Label>
+                <Input name={inputNames.FIRST_NAME}
+                  id="create-user-firstname-input"
+                  className="responsive-modal-row-item"
+                  placeholder="First Name"
+                  onChange={this.updateNewUserInfo}
+                  value={createUser.newUser.firstName}
+                  valid={!!createUser.newUser.firstName}
+                  invalid={!createUser.newUser.firstName} />
+              </div>
 
-              <Input name={inputNames.LAST_NAME}
-                className="responsive-modal-row-item"
-                placeholder="Last Name"
-                onChange={this.updateNewUserInfo}
-                value={createUser.newUser.lastName}
-                valid={!!createUser.newUser.lastName}
-                invalid={!createUser.newUser.lastName} />
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-lastname-input">Last Name</Label>
+                <Input name={inputNames.LAST_NAME}
+                  id="create-user-lastname-input"
+                  className="responsive-modal-row-item"
+                  placeholder="Last Name"
+                  onChange={this.updateNewUserInfo}
+                  value={createUser.newUser.lastName}
+                  valid={!!createUser.newUser.lastName}
+                  invalid={!createUser.newUser.lastName} />
+              </div>
             </div>
             <div className="responsive-modal-row">
-              {/* <InputGroup className="responsive-modal-row-item">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Email</InputGroupText>
-                </InputGroupAddon> */}
-              <Input className="responsive-modal-row-item"
-                name={inputNames.EMAIL}
-                onChange={this.updateNewUserInfo}
-                value={createUser.newUser.email}
-                valid={!!createUser.newUser.email}
-                invalid={!createUser.newUser.email}
-                placeholder="Email" />
-              <Input className="responsive-modal-row-item"
-                name={inputNames.PHONE}
-                onChange={this.updateNewUserInfo}
-                value={createUser.newUser.phoneNumber}
-                valid={!!createUser.newUser.phoneNumber}
-                invalid={!createUser.newUser.phoneNumber}
-                placeholder="Phone Number" />
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-email-input">Email</Label>
+                <Input className="responsive-modal-row-item"
+                  id="create-user-email-input"
+                  name={inputNames.EMAIL}
+                  onChange={this.updateNewUserInfo}
+                  value={createUser.newUser.email}
+                  valid={!!createUser.newUser.email}
+                  invalid={!createUser.newUser.email}
+                  placeholder="Email" />
+              </div>
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-phoneNumber-input">Phone Number</Label>
+                <Input className="responsive-modal-row-item"
+                  id="create-user-phoneNumber-input"
+                  name={inputNames.PHONE}
+                  onChange={this.updateNewUserInfo}
+                  value={createUser.newUser.phoneNumber}
+                  valid={!!createUser.newUser.phoneNumber}
+                  invalid={!createUser.newUser.phoneNumber}
+                  placeholder="Phone Number" />
+              </div>
             </div>
-            <div className="responsive-modal-row">
-              {/*<InputGroup >
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Phone Number</InputGroupText>
-                </InputGroupAddon> */}
-              <Dropdown color="success" className="responsive-modal-row-item rev-btn"
-                isOpen={this.props.createUser.locationDropdownActive}
-                toggle={this.props.toggleLocationDropdown}>
-                <DropdownToggle caret>
-                  {createUser.newUser.trainingAddress.alias || 'Location'}
-                </DropdownToggle>
-                <DropdownMenu>
-                  {
-                    addresses.trainingAddresses.length === 0
-                      ? <>
-                        <DropdownItem>Unable To Find Any Locations</DropdownItem>
-                        <DropdownItem divider />
-                      </>
-                      : addresses.trainingAddresses.map(location =>
-                        <DropdownItem key={location.addressId} onClick={() => this.props.updateNewUserLocation(location)}>{location.alias}</DropdownItem>
-                      )
-                  }
-                </DropdownMenu>
-              </Dropdown>
-
-              <Dropdown color="success" className="responsive-modal-row-item rev-btn"
-                isOpen={this.props.createUser.roleDropdownActive}
-                toggle={this.props.toggleRoleDropdown}>
-                <DropdownToggle caret>
-                  {createUser.newUser.dropdownRole || 'Role'}
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem onClick={() => this.props.updateNewUserRole('admin', 'Admin')}>Admin</DropdownItem>
-                  <DropdownItem onClick={() => this.props.updateNewUserRole('staging-manager', 'Staging Manager')}>Staging Manager</DropdownItem>
-                  <DropdownItem onClick={() => this.props.updateNewUserRole('trainer', 'Trainer')}>Trainer</DropdownItem>
-                  <DropdownItem onClick={() => this.props.updateNewUserRole('associate', 'Associate')}>Associate</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-
-              <Dropdown color="success" className="responsive-modal-row-item rev-btn"
-                isOpen={this.props.createUser.cohortDropdownActive}
-                toggle={this.props.toggleCohortDropdown}
-              >
-                <DropdownToggle caret>
-                  {createUser.newUser.cohort.cohortName || 'Cohort'}
-                </DropdownToggle>
-                <DropdownMenu>
-                  {
-                    cohorts.cohorts.length === 0
-                      ? <>
-                        <DropdownItem>Unable To Find Any Cohorts</DropdownItem>
-                        <DropdownItem divider />
-                      </>
-                      : cohorts.cohorts.map(cohort =>
-                        <DropdownItem key={cohort.cohortId} onClick={() => this.props.updateNewUserCohort(cohort)}>{cohort.cohortName}</DropdownItem>
-                      )
-                  }
-                </DropdownMenu>
-              </Dropdown>
+            <div className="responsive-modal-row create-user-buttons">
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-location-dropdown">Location</Label>
+                <Dropdown color="success" className="responsive-modal-row-item rev-btn"
+                  id="create-user-location-dropdown"
+                  isOpen={this.props.createUser.locationDropdownActive}
+                  toggle={this.props.toggleLocationDropdown}>
+                  <DropdownToggle caret>
+                    {createUser.newUser.trainingAddress.alias || 'Location'}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    {
+                      addresses.trainingAddresses.length === 0
+                        ? <>
+                          <DropdownItem>Unable To Find Any Locations</DropdownItem>
+                          <DropdownItem divider />
+                        </>
+                        : addresses.trainingAddresses.map(location =>
+                          <DropdownItem key={location.addressId} onClick={() => this.props.updateNewUserLocation(location)}>{location.alias}</DropdownItem>
+                        )
+                    }
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-role-dropdown">Role</Label>
+                <Dropdown color="success" className="responsive-modal-row-item rev-btn"
+                  id="create-user-role-dropdown"
+                  isOpen={this.props.createUser.roleDropdownActive}
+                  toggle={this.props.toggleRoleDropdown}>
+                  <DropdownToggle caret>
+                    {createUser.newUser.dropdownRole || 'Role'}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={() => this.props.updateNewUserRole('admin', 'Admin')}>Admin</DropdownItem>
+                    <DropdownItem onClick={() => this.props.updateNewUserRole('staging-manager', 'Staging Manager')}>Staging Manager</DropdownItem>
+                    <DropdownItem onClick={() => this.props.updateNewUserRole('trainer', 'Trainer')}>Trainer</DropdownItem>
+                    <DropdownItem onClick={() => this.props.updateNewUserRole('associate', 'Associate')}>Associate</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
+              <div className="responsive-modal-column create-user-margin">
+                <Label for="create-user-cohort-dropdown">Cohort</Label>
+                <Dropdown color="success" className="responsive-modal-row-item rev-btn"
+                  id="create-user-cohort-dropdown"
+                  isOpen={this.props.createUser.cohortDropdownActive}
+                  toggle={this.props.toggleCohortDropdown}>
+                  <DropdownToggle caret>
+                    {createUser.newUser.cohort.cohortName || 'Cohort'}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    {
+                      cohorts.cohorts.length === 0
+                        ? <>
+                          <DropdownItem>Unable To Find Any Cohorts</DropdownItem>
+                          <DropdownItem divider />
+                        </>
+                        : cohorts.cohorts.map(cohort =>
+                          <DropdownItem key={cohort.cohortId} onClick={() => this.props.updateNewUserCohort(cohort)}>{cohort.cohortName}</DropdownItem>
+                        )
+                    }
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
             </div>
           </ModalBody>
           <ModalFooter id="create-user-modal-footer">
